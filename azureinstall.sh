ff298487d0df26c1a6793d0bffff77e07ee47c5f
wantonsolutions <stewbertgrant@gmail.com>
2017-01-03 16:16:07 -0800

    azure install script

---
+++ azureinstall.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+#go to home directory
+cd
+
+#download go binary
+wget https://storage.googleapis.com/golang/go1.7.4.linux-amd64.tar.gzsu
+
+#unzip and remove
+sudo tar -C /usr/local -xzf go1.7.4.linux-amd64.tar.gz
+rm go1.7.4.linux-amd64.tar.gz
+
+#export path
+export PATH=$PATH:/usr/local/go/bin
+echo "" >> .profile
+echo "#export go path" >> .profile
+echo "export PATH=$PATH:/usr/local/go/bin" >> .profile
+
+#make root directory and set GOPATH
+mkdir go
+export GOPATH=$HOME/go
+echo "" >> .profile
+echo "#set GOPATH" >> .profile
+echo "export GOPATH=$HOME/go" >> .profile
+
+#export workspace bin
+export PATH=$PATH:$GOPATH/bin
+echo "" >> .profile
+echo "#set local bin" >> .profile
+echo "export PATH=$PATH:$GOPATH/bin" >> .profile
+
+#install mercurial
+sudo apt-get install mercurial
+
+#install GoVector to boot strap directories
+go get github.com/arcaneiceman/GoVector
