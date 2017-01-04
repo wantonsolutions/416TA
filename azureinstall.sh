7f88000ff19fecde0ab21e28464a9878c05a27af
wantonsolutions <stewbertgrant@gmail.com>
2017-01-04 14:11:09 -0800

    fixed small bug

--- azureinstall.sh
+++ azureinstall.sh
@@ -4,7 +4,7 @@
 cd
 
 #download go binary
-wget https://storage.googleapis.com/golang/go1.7.4.linux-amd64.tar.gzsu
+wget https://storage.googleapis.com/golang/go1.7.4.linux-amd64.tar.gz
 
 #unzip and remove
 sudo tar -C /usr/local -xzf go1.7.4.linux-amd64.tar.gz
